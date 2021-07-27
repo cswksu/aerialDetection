d3a623cf498dd597c5b51d6a8c413bba1aba29fd
cswksu@gmail.com <cswksu@gmail.com>
2021-07-26 19:09:53 -0700

    add channel depth setting
    also add torch.no_grad context

--- inference/inference.py
+++ inference/inference.py
@@ -6,13 +6,17 @@ import torch.nn as nn
 
 
 class unet_inference():
-    def __init__(self, path_to_state_dict):
+    def __init__(self, path_to_state_dict, num_out_channels=2):
         state_dict = torch.load(path_to_state_dict, map_location = torch.device('cpu'))
         self.model = ResUNet(num_blocks=4)
         self.model.load_state_dict(state_dict)
         self.softmax = nn.Softmax(dim=0)
+        self.num_out_channels = num_out_channels
         self.model.eval()
-    
+
+    def set_output_channels(self, num_out_channels):
+        self.num_out_channels = num_out_channels
+
     def run_inference(self, input):
         """Runs inference on input data
         
@@ -22,9 +26,12 @@ class unet_inference():
         prob(~building) and 1 channel is prob(building)."""
         assert(len(input.shape)==3)
         #transform
-        input = TF.normalize(input, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        input = input.unsqueeze(0)
-        output = self.model.forward(input)
-        output = output.squeeze()
-        output = self.softmax(output)
-        return output
+        with torch.no_grad():
+            input = TF.normalize(input, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            input = input.unsqueeze(0)
+            output = self.model.forward(input)
+            output = output.squeeze()
+            output = self.softmax(output)
+            if self.num_out_channels == 1:
+                output = (torch.lt(output[0, :, :],output[1, :, :])).float()
+            return output
